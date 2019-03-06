70a1c79cf3d953ab0b12ce52d3f1703defbd044e
Amelie Oller <amelieoller@gmail.com>
2019-03-06 12:27:18 -0800

    Remove main character from lab, in show.rb and spec file

--- lib/show.rb
+++ lib/show.rb
@@ -3,17 +3,14 @@ require "pry"
 # shows = [
 # 	{
 # 		name: "Friends",
-# 		main_character: "Rachel Green",
 # 		rating: 8
 # 	},
 # 	{
 # 		name: "Rick and Morty",
-# 		main_character: "Rick",
 # 		rating: 10
 # 	},
 # 	{
 # 		name: "Breaking Bad",
-# 		main_character: "Walter White",
 # 		rating: 9
 # 	}
 # ]
--- spec/show_spec.rb
+++ spec/show_spec.rb
@@ -1,17 +1,15 @@
 require_relative "../lib/show.rb"
 
 describe "show" do
-	# Array of show hashes, where each show has a name, main character and rating.
+	# Array of show hashes, where each show has a name and a rating.
   let(:shows) {
   	[
 			{
 				name: "Friends",
-				main_character: "Rachel Green",
 				rating: 8
 			},
 			{
 				name: "Rick and Morty",
-				main_character: "Rick",
 				rating: 10
 			}
 		]
@@ -19,8 +17,8 @@ describe "show" do
 	
 	# Write a method that returns the number of shows in your array
 		context "#show_count" do
-		let(:breaking_bad) { { name: "Breaking Bad", main_character: "Walter White", rating: 9 } }
-		let(:home_movies) { { name: "Home Movies", main_character: "Brandon", rating: 8 } }
+		let(:breaking_bad) { { name: "Breaking Bad", rating: 9 } }
+		let(:home_movies) { { name: "Home Movies", rating: 8 } }
 
 		it 'defines a method show_count' do
 			expect(defined?(show_count)).to be_truthy
@@ -37,35 +35,35 @@ describe "show" do
 
  	# Write a method that adds a new show to the shows array
   context "#add_show" do
-		let(:breaking_bad) { { name: "Breaking Bad", main_character: "Walter White", rating: 9 } }
-		let(:home_movies) { { name: "Home Movies", main_character: "Brandon", rating: 8 } }
+		let(:breaking_bad) { { name: "Breaking Bad", rating: 9 } }
+		let(:home_movies) { { name: "Home Movies", rating: 8 } }
 
 		it 'defines a method add_show' do
 			expect(defined?(add_show)).to be_truthy
 		end
 
-    it "accepts 4 arguments, all shows, the name of the new show, the name of the main character, and a rating and adds that new show to the shows array" do
-      add_show(shows, "Breaking Bad", "Walter White", 9)
+    it "accepts 3 arguments, all shows, the name of the new show, and that shows rating and adds that new show to the shows array" do
+      add_show(shows, "Breaking Bad", 9)
       expect(shows).to include(breaking_bad)
       expect(shows.length).to be(3)
     end
 
     it "adds a different show to the shows array" do
-      add_show(shows, "Home Movies", "Brandon", 8)
-			add_show(shows, "Breaking Bad", "Walter White", 9)
+      add_show(shows, "Home Movies", 8)
+			add_show(shows, "Breaking Bad", 9)
 			expect(shows).to include(home_movies)
       expect(shows.length).to be(4)
 		end
 		
 		it "prints that the show was added and uses the show_count method to print out how many shows are in the array" do
 			expect($stdout).to receive(:puts).with("Home Movies has been added to the list! You now have 3 show(s) in your list.")
-			add_show(shows, "Home Movies", "Brandon", 8)
+			add_show(shows, "Home Movies", 8)
 		end
 		
 		it 'calls show_count passing in the shows' do
 			allow($stdout).to receive(:puts)
 			expect(self).to receive(:show_count).with(shows).at_least(:once)
-			add_show(shows, "Breaking Bad", "Walter White", 9)
+			add_show(shows, "Breaking Bad", 9)
 		end
 	end
 
@@ -83,8 +81,8 @@ describe "show" do
 
 		it "should output a formatted list of shows and their ratings" do
 			shows = [
-				{name: "Friends", main_character: "Rachael", rating: 9},
-				{name: "Rick and Morty", main_character: "Rick", rating: 10}
+				{name: "Friends", rating: 9},
+				{name: "Rick and Morty", rating: 10}
 			]
 
 			# For more info about heredocs, see this link: http://en.wikibooks.org/wiki/Ruby_Programming/Here_documents
